8b079df99232e9b0b7ef707a722f721469842fed
squallqi <squallqi@126.com>
2017-02-28 16:13:08 +0800

    更新 models.py

--- CMDB/CMDB/models.py
+++ CMDB/CMDB/models.py
@@ -1,27 +1,127 @@
-#!/usr/bin/env python
-#coding:utf-8
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
-class UserInfo(models.Model):
-    usename = models.CharField(max_length=50)
-    passwd = models.CharField(max_length=15)
-    memo = models.CharField(max_length=15)
-class Group(models.Model):
-    Name = models.CharField(max_length=50)
-class User(models.Model):
-    Name = models.CharField(max_length=50)
-    Email = models.CharField(max_length=50)
-    group_relation = models.ManyToManyField('Group')
-class Args(models.Model):
-    name = models.CharField(max_length=20,null=True)
-    not_name = models.CharField(max_length=20,null=False)
-class Asset(models.Model):
-    hostname = models.CharField(max_length=256)
-    create_date = models.DateField(auto_now_add=True)
-    update_date = models.DateField(auto_now=True)
-class UserInfo_Temp(models.Model):
-    GENDER_CHOICE = (
-        (u'1',u'普通用户'),
-        (u'2',u'管理员'),
-        (u'3',u'超级用户'),
-    )
-    UserType = models.CharField(max_length=2,choices=GENDER_CHOICE)
+
+
+class ISP(models.Model):
+    name = models.CharField(max_length=255, verbose_name='名称')
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "ISP类型"
+        verbose_name_plural = verbose_name
+
+
+class IDCLevel(models.Model):
+    name = models.CharField(max_length=255, verbose_name='名称')
+    comment = models.TextField(verbose_name='描述')
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "机房等级"
+        verbose_name_plural = verbose_name
+
+
+class IDC(models.Model):
+    name = models.CharField(max_length=255, verbose_name='机房名称')
+    bandwidth = models.CharField(max_length=255, blank=True, null=True, verbose_name='机房带宽')
+    phone = models.CharField(max_length=255, verbose_name='联系电话')
+    linkman = models.CharField(max_length=255, null=True, verbose_name='联系人')
+    address = models.CharField(max_length=255, blank=True, null=True, verbose_name="机房地址")
+    concat_email = models.EmailField(verbose_name='联系邮箱', blank=True, null=True, default="")
+    network = models.TextField(blank=True, null=True, verbose_name="IP地址段")
+    create_time = models.DateField(auto_now=True, verbose_name='创建时间')
+    operator = models.ForeignKey(ISP, verbose_name='ISP类型')
+    type = models.ForeignKey(IDCLevel, verbose_name='机房类型')
+    comment = models.TextField(blank=True, null=True, verbose_name="备注")
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "机房"
+        verbose_name_plural = verbose_name
+
+
+class Cabinet(models.Model):
+    idc = models.ForeignKey(IDC, verbose_name='机房')
+    name = models.CharField(max_length=30, unique=True, verbose_name="机柜编号")
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "机柜"
+        verbose_name_plural = verbose_name
+
+
+class Rack(models.Model):
+    cabinet = models.ForeignKey(Cabinet, verbose_name='机柜')
+    name = models.CharField(max_length=30, unique=True, verbose_name="机架名称")
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "机架"
+        verbose_name_plural = verbose_name
+
+
+class Host(models.Model):
+    host_name = models.CharField(max_length=255, blank=True, null=True, verbose_name="主机DNS名称")
+    kernel = models.CharField(max_length=255, blank=True, null=True, verbose_name="系统内核")
+    kernel_release = models.CharField(max_length=255, blank=True, null=True, verbose_name="系统内核版本")
+    virtual = models.CharField(max_length=255, blank=True, null=True, verbose_name="设备类型")
+    host = models.CharField(max_length=255, blank=True, null=True, verbose_name="主机名")
+    osrelease = models.CharField(max_length=255, blank=True, null=True, verbose_name="操作系统版本")
+    saltversion = models.CharField(max_length=255, blank=True, null=True, verbose_name="Salt版本")
+    osfinger = models.CharField(max_length=255, blank=True, null=True, verbose_name="系统指纹")
+    os_family = models.CharField(max_length=255, blank=True, null=True, verbose_name="系统类型")
+    num_gpus = models.IntegerField(blank=True, null=True, verbose_name="GPU数量")
+    system_serialnumber = models.CharField(max_length=255, blank=True, null=True, verbose_name="SN号")
+    cpu_model = models.CharField(max_length=255, blank=True, null=True, verbose_name="CPU型号")
+    productname = models.CharField(max_length=255, blank=True, null=True, verbose_name="产品名称")
+    osarch = models.CharField(max_length=255, blank=True, null=True, verbose_name="系统架构")
+    cpuarch = models.CharField(max_length=255, blank=True, null=True, verbose_name="CPU架构")
+    os = models.CharField(max_length=255, blank=True, null=True, verbose_name="操作系统")
+    mem_total = models.IntegerField(blank=True, null=True, verbose_name="内存大小")
+    num_cpus = models.IntegerField(blank=True, null=True, verbose_name="CPU数量")
+    rack = models.ForeignKey(Rack, verbose_name='机架', blank=True, null=True)
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.host_name
+
+    class Meta:
+        verbose_name = "主机"
+        verbose_name_plural = verbose_name
+
+
+class HostIP(models.Model):
+    ip = models.CharField(max_length=255, blank=True, null=True, verbose_name="IP地址")
+    host = models.ForeignKey(Host, default="", verbose_name="主机", blank=True, null=True, )
+    create_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    update_time = models.DateTimeField(verbose_name='更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.ip
+
+    class Meta:
+        verbose_name = "主机IP"
+        verbose_name_plural = verbose_name
