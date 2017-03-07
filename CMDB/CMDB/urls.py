3484237fe58185504fdf6f243625c10bec332700
root <squallqi@126.com>
2017-03-08 07:14:59 +0800

    update

--- CMDB/CMDB/urls.py
+++ CMDB/CMDB/urls.py
@@ -1,29 +1,36 @@
-"""CMDB URL Configuration
+#coding:utf-8
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sessions import serializers
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from rest_framework import routers
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
 
 
 urlpatterns = [
-    # url(r'^api/', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     #url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     #url(r'^jet/', include('jet.urls', 'jet')),
@@ -32,4 +39,5 @@ urlpatterns = [
 ]
 admin.site.site_header = u'运维平台'
 admin.site.site_title = u'运维平台'
+
 
