d8ade4858834bd1b2e4ce2c9a863fbcf0fe815fb
squallqi <squallqi@126.com>
2017-03-24 15:49:26 +0800

    更新remote.py

--- useful/remote/remote.py
+++ useful/remote/remote.py
@@ -11,9 +11,9 @@ cf.read('config.ini')
 #address = master.split(',')
 #print  address
 
-username = 'root'
-password = '1'
-port = 22
+username = cf.get('Host','username')
+password = cf.get('Host','password')
+port = cf.getint('Host','port')
 host = {
     'master': cf.get('Host','master'),
     'as': cf.get('Host', 'as'),
@@ -24,13 +24,15 @@ if __name__=='__main__':
     for i in host.keys():
         hostname = i
         address = host[i]
-        #print address
+        print address
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
             client.connect(address, port, username, password, look_for_keys=False)
             #paramiko.util.log_to_file("filename.log")
-            stdin, stdout, stderr = client.exec_command('echo %s > /etc/hostname' % hostname)
+            stdin, stdout, stderr = client.exec_command('echo %s > /etc/hostname|hostname %s' % (hostname,hostname))
+            #stdin, stdout, stderr = client.exec_command('hostname %s' % hostname)
+
             print stdout.read(),
         except Exception, e:
             print e
